a273cf31c46a59c67d40928702af12cf722bfd23
Claxon <25482509+clausbertels@users.noreply.github.com>
2024-11-28 19:38:24 +0100

    Initial Commit

---
+++ IMUI/IMUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMUIApp.swift
+//  IMUI
+//
+//  Created by Claus Bertels on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct IMUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
